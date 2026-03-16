171c7fe4259134bc927ff89bcd59a33a0f9ee2ac
rusuvlad880 <vlad.rusu13@gmai.com>
2026-03-16 12:13:28 +0100

    Created the database for the app

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
 import customtkinter as ctk
+import database
+
+# Just to be sure that the table exists before the app starts
+
+database.create_table()
 
 #Set the overall theme and color of the app
 
@@ -16,10 +21,34 @@ app.title("Backlog Gatekeeper")
 welcome_label = ctk.CTkLabel(app, text="Welcome to the Backlog Gatekeeper!", font=("Arial", 20, "bold"))
 welcome_label.pack(pady=40)
 
-#Dummy button 
+title_entry = ctk.CTkEntry(app, placeholder_text="Game Title", width=250)
+title_entry.pack(pady=10)
 
-add_button = ctk.CTkButton(app, text="Add New Game")
-add_button.pack(pady=10)
+platform_entry = ctk.CTkEntry(app, placeholder_text="Platform you own the game", width=250)
+platform_entry.pack(pady=10)
+
+status_entry = ctk.CTkEntry(app, placeholder_text="Status of your game", width=250)
+status_entry.pack(pady=10)
+
+def save_button_clicked():
+
+    title = title_entry.get()
+    platform = platform_entry.get()
+    status = status_entry.get()
+
+    if title and platform and status:
+        database.add_game(title, platform, status)
+        print(f"Success! Saved {title} to the database.")
+
+        title_entry.delete(0, ctk.END)
+        platform_entry.delete(0, ctk.END)
+        status_entry.delete(0, ctk.END)
+
+    else:
+        print("Please fill in all the fields!")
+
+add_button = ctk.CTkButton(app, text="Save Game", command=save_button_clicked)
+add_button.pack(pady=20)
 
 #Run
 
---
+++ database.py
@@ -0,0 +1,39 @@
+import sqlite3
+
+def create_table():
+
+    #Creates file called backlog.db if it doesn't exist and connects to it
+
+    conn = sqlite3.connect('backlog.db')
+    cursor = conn.cursor()
+
+    #SQL command creates games table with 4 columns
+
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS games(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            platform TEXT NOT NULL,
+            status TEXT NOT NULL       
+        )
+    ''')
+
+    #Save changes and close the connection
+    conn.commit()
+    conn.close()
+
+def add_game(title, platform, status):
+
+    conn = sqlite3.connect('backlog.db')
+    cursor = conn.cursor()
+
+    cursor.execute('''
+        INSERT INTO games (title, platform, status)
+        VALUES (?, ?, ?)
+    ''', (title, platform, status))
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    create_table()
+    print("Database and 'games' table created succesfully!")
